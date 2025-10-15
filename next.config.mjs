77176bd203aa6a00474b7bf2f0ba36e15df6a6b1
Aleksey1989god <98532149+Aleksey1989god@users.noreply.github.com>
2025-10-15 18:13:05 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Важно для GitHub Pages
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Важно для GitHub Pages
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
